cba91c4349f3a036bda58f0c8eb4b5104125936c
Nur Fidiya <96903083+fidya02@users.noreply.github.com>
2024-03-15 15:19:33 +0700

    Create 2_2_Code_Hello_World(Fidiya_12211858).py

---
+++ Tugas 2_BigData_NurFidiya_12211858/2_2_Code_Hello_World(Fidiya_12211858).py
@@ -0,0 +1,56 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Code Hello World
+# MAGIC Struktur code pada Python relatif sangat sederhana dan tidak terlalu rumit seperti pada Java ataupun C. Selain karena memang mudah dibaca, python juga termasuk pada high level programming language. Sebagai contoh, cobalah ketik coding pada Code Editor dengan perintah berikut untuk mengeluarkan kata-kata yang diperintahkan. Ekskusilah perintah dari kode dibawah ini.
+
+# COMMAND ----------
+
+print ("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Coba lanjutkan code tersebut pada baris selanjutnya dengan menggunakan kode ini :
+
+# COMMAND ----------
+
+print ("Hello World")
+print(5+1)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ***Note : setiap print akan mencetak baris baru.***
+# MAGIC
+# MAGIC Memang terlihat sangat sederhana jika hanya untuk kasus sederhana yang kita pelajari pada section kali ini. Namun, python tidak sesederhana itu. Biasanya kita juga menggunakan import library khusus jika sudah menyangkut masalah yang lebih kompleks. Materi berikutnya mungkin akan menjelaskan lebih lanjut.
+# MAGIC
+# MAGIC  
+# MAGIC
+# MAGIC **Tugas Praktek 3.1**  
+# MAGIC 1. Tampilkan hasil perhitungan (10*2+5) pada baris pertama.
+
+# COMMAND ----------
+
+# Jawab Tugas Praktek 3.1 Nomor 1
+print('hitung 10x2+5')
+print(10*2+5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 2. tampilkan teks "2023 Aku kaya mulia" pada baris kedua.
+
+# COMMAND ----------
+
+# Jawab Tugas Praktek 3.1 Nomor 2
+print('2024 Aku kaya, mulia')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 3. Tampilkan teks Nama anda
+
+# COMMAND ----------
+
+# Jawab Tugas Praktek 3.1 Nomor 3
+print('Nur Fidiya')
